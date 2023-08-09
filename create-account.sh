6afb6c7b1a3cd7fd60978db56a41b0df3d55cf30
MTGofa <gofa_25@hotmail.com>
2023-08-09 14:29:19 +0300

    save

---
+++ create-account.sh
@@ -0,0 +1,84 @@
+#!/bin/sh
+
+username=$1
+version=${2:-'8.1'}
+ownership=${3:-'test'}
+dir=${4:-'public'}
+domain=$username.$ownership
+
+root="/var/www/$username/$dir"
+parent="/var/www/$username"
+block="/etc/nginx/sites-available/$username"
+
+# # Create the Document Root directory
+#mkdir -p $root
+#sudo chmod -R 755 $parent
+# # Assign ownership to your regular user account
+#sudo chown -R $USER:$USER $parent
+
+# # Create the Nginx server block file:
+sudo tee $block > /dev/null <<EOF
+server {
+    listen 80;
+    listen [::]:80;
+    server_name $domain *.$domain;
+    listen 443 ssl;
+    ssl_certificate /etc/ssl/certs/nginx-selfsigned.crt;
+    ssl_certificate_key /etc/ssl/private/nginx-selfsigned.key;
+    root $root;
+    index index.html index.htm index.php index.nginx-debian.html;
+
+    location / {
+        try_files \$uri \$uri/ /index.php\$is_args\$args;
+        autoindex on;
+        #proxy_bind 127.0.0.1;
+        #proxy_pass http://127.0.0.1:3000;
+        #proxy_set_header    Upgrade \$http_upgrade;
+        #proxy_set_header    Connection 'upgrade';
+        #proxy_set_header    X-Forwarded-For \$remote_addr;
+        #proxy_set_header    Host \$host;
+        #proxy_http_version  1.1;
+    }
+
+    location ~ \.php$ {
+        fastcgi_pass unix:/run/php/php$version-fpm.sock;
+        fastcgi_param SCRIPT_FILENAME $document_root\$fastcgi_script_name;
+        include fastcgi_params;
+        include snippets/fastcgi-php.conf;
+    }
+
+    # A long browser cache lifetime can speed up repeat visits to your page
+    location ~* \.(jpg|jpeg|gif|png|webp|svg|woff|woff2|ttf|css|js|ico|xml)$ {
+        access_log off;
+        log_not_found off;
+        expires 360d;
+    }
+
+    # disable access to hidden files
+    location ~ /\.ht {
+        access_log off;
+        log_not_found off;
+        deny all;
+    }
+}
+EOF
+
+# sudo tee $root'/index.html' > /dev/null <<EOF
+# <center><h1>Welcome $username</h1></center> 
+# EOF
+
+# # Link to make it available
+# # sudo ln -s $block /etc/nginx/sites-enabled/
+
+
+sudo echo "127.0.0.1 $domain" >> /etc/hosts
+# sudo echo "127.0.0.1 *.$domain" >> /etc/hosts
+
+# # Test configuration and reload if successful
+sudo nginx -t && sudo service nginx reload
+
+#sudo chmod -R 777 /var/www
+
+printf "\n\n *** Created successfully ***\n\n"
+
+exit
---
+++ install.sh
@@ -0,0 +1,174 @@
+#!/bin/sh
+
+script_log_file="script_log.log"
+green_color="\033[1;32m"
+no_color="\033[0m"
+current_user=$(logname)
+
+echo $no_color"PREPAIRE INSTALLING";
+rm -rf /var/lib/dpkg/lock >> $script_log_file 2>/dev/null
+rm -rf /var/lib/dpkg/lock-frontend >> $script_log_file 2>/dev/null
+rm -rf /var/cache/apt/archives/lock >> $script_log_file 2>/dev/null
+sudo apt-get update  >> $script_log_file 2>/dev/null
+echo $green_color"[SUCCESS]";
+echo $green_color"[######################################]";
+
+echo $no_color"REMOVING APACHE";
+sudo apt-get purge apache -y >> $script_log_file 2>/dev/null
+sudo apt-get purge apache* -y >> $script_log_file 2>/dev/null
+sudo kill -9 $(sudo lsof -t -i:80) >> $script_log_file 2>/dev/null
+sudo kill -9 $(sudo lsof -t -i:443) >> $script_log_file 2>/dev/null
+echo $green_color"[SUCCESS]";
+echo $green_color"[######################################]";
+
+echo $no_color"INSTALLING NGINX";
+sudo apt-get update   >> $script_log_file 2>/dev/null
+sudo apt install nginx -y >> $script_log_file 2>/dev/null
+echo $green_color"[SUCCESS]";
+echo $green_color"[######################################]";
+
+
+for version in 8.2 8.1 7.4;
+do
+    echo $no_color"INSTALLING PHP "$version;
+    sudo apt-get update  >> $script_log_file 2>/dev/null
+    sudo apt install lsb-release ca-certificates apt-transport-https software-properties-common -y >> $script_log_file 2>/dev/null
+    sudo add-apt-repository ppa:ondrej/php -y >> $script_log_file 2>/dev/null
+    sudo apt-get update  >> $script_log_file 2>/dev/null
+    sudo apt install php$version -y >> $script_log_file 2>/dev/null
+    
+    echo $no_color"INSTALLING PHP EXTENSIONS";
+    sudo apt install php$version openssl php$version-fpm php$version-common php$version-curl php$version-mbstring php$version-mysql php$version-xml php$version-zip php$version-gd php$version-cli php$version-xml php$version-imagick php$version-xml php$version-intl php-mysql -y >> $script_log_file 2>/dev/null
+
+    echo $green_color"CHANGING PHP FPM UPLOAD VALUES";
+    sudo sed -i 's/post_max_size = 8M/post_max_size = 1000M/g' /etc/php/$version/fpm/php.ini >> $script_log_file 2>/dev/null
+    sudo sed -i 's/upload_max_filesize = 2M/upload_max_filesize = 1000M/g' /etc/php/$version/fpm/php.ini >> $script_log_file 2>/dev/null
+    sudo sed -i 's/max_execution_time = 30/max_execution_time = 300/g' /etc/php/$version/fpm/php.ini >> $script_log_file 2>/dev/null
+    sudo sed -i 's/memory_limit = 128/memory_limit = 12800/g' /etc/php/$version/fpm/php.ini >> $script_log_file 2>/dev/null
+    sudo sed -i 's/user = www-data/user = '$current_user'/g' /etc/php/$version/fpm/pool.d/www.conf >> $script_log_file 2>/dev/null
+    sudo service php$version-fpm restart >> $script_log_file 2>/dev/null
+    echo $green_color"[SUCCESS]";
+    echo $green_color"[######################################]";
+
+done
+
+
+echo $green_color"[######################################]";
+echo $no_color"INSTALLING COMPOSER";
+sudo apt-get update  >> $script_log_file 2>/dev/null
+sudo apt-get purge composer -y >> $script_log_file 2>/dev/null
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');" >> $script_log_file 2>/dev/null
+php composer-setup.php >> $script_log_file  2>/dev/null
+sudo mv composer.phar /usr/local/bin/composer >> $script_log_file 2>/dev/null
+echo $green_color"[SUCCESS]";
+echo $green_color"[######################################]";
+
+
+echo $no_color"RESTARTING NGINX";
+sudo pkill -f nginx & wait $! >> $script_log_file 2>/dev/null
+sudo systemctl start nginx >> $script_log_file 2>/dev/null
+sudo service nginx restart >> $script_log_file 2>/dev/null
+echo $green_color"[SUCCESS]";
+echo $green_color"[######################################]";
+
+echo $no_color"CREATING NGINX CONFIGURATION";
+sudo rm -rf /etc/nginx/sites-available/default /etc/nginx/sites-enabled >> $script_log_file 2>/dev/null
+sudo ln -s /etc/nginx/sites-available /etc/nginx/sites-enabled >> $script_log_file 2>/dev/null
+
+sudo touch /etc/nginx/conf.d/default.conf >> $script_log_file 2>/dev/null
+sudo bash -c "echo 'server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    listen 443 ssl;
+    ssl_certificate /etc/ssl/certs/nginx-selfsigned.crt;
+    ssl_certificate_key /etc/ssl/private/nginx-selfsigned.key;
+
+    root /var/www/html;
+    index index.html index.htm index.nginx-debian.html index.php;
+    server_name _;
+
+    location / {
+        try_files \$uri \$uri/ =404;
+    }
+
+    location ~ \.php$ {
+        fastcgi_pass unix:/run/php/php8.1-fpm.sock;
+        fastcgi_param SCRIPT_FILENAME \$fastcgi_script_name;
+        include fastcgi_params;
+        include snippets/fastcgi-php.conf;
+    }
+
+    location /phpmyadmin {
+        root /usr/share/;
+        index index.php index.html index.htm;
+        location ~ ^/phpmyadmin/(.+\.php)$ {
+            try_files \$uri =404;
+            root /usr/share/;
+            fastcgi_pass unix:/run/php/php8.1-fpm.sock;
+            fastcgi_index index.php;
+            fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
+            include /etc/nginx/fastcgi_params;
+        }
+        location ~* ^/phpmyadmin/(.+\.(jpg|jpeg|gif|css|png|js|ico|html|xml|txt))$ {
+            root /usr/share/;
+        }
+    }
+}' > /etc/nginx/conf.d/default.conf" >> $script_log_file 2>/dev/null
+
+sudo chown -R ${USER}:${USER} /var/www >> $script_log_file 2>/dev/null
+sudo chmod -R 755 /var/www >> $script_log_file 2>/dev/null
+
+echo $green_color"[SUCCESS]";
+echo $green_color"[######################################]";
+
+
+echo $no_color"RESTARTING NGINX";
+sudo pkill -f nginx & wait $! >> $script_log_file 2>/dev/null
+sudo systemctl start nginx >> $script_log_file 2>/dev/null
+sudo service nginx restart >> $script_log_file 2>/dev/null
+echo $green_color"[SUCCESS]";
+echo $green_color"[######################################]";
+
+if ! [ -x "$(command -v mysql)"  >> $script_log_file 2>/dev/null ]; then
+echo $no_color"INSTALLING MYSQL";
+sudo apt-get -qq install mysql-server  >> $script_log_file 2>/dev/null
+echo $green_color"[SUCCESS]";
+echo $green_color"[######################################]";
+fi
+
+echo $no_color"Configure MYSQL";
+sudo mysql > /dev/null << EOF 
+ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '';
+FLUSH PRIVILEGES;
+EOF
+echo $green_color"[SUCCESS]";
+echo $green_color"[######################################]";
+
+
+echo $no_color"INSTALLING git";
+sudo apt-get update   >> $script_log_file 2>/dev/null
+sudo apt install git -y >> $script_log_file 2>/dev/null
+echo $green_color"[SUCCESS]";
+echo $green_color"[######################################]";
+
+echo $no_color"INSTALLING NPM";
+sudo apt install npm -y >> $script_log_file 2>/dev/null
+echo $green_color"[SUCCESS]";
+echo $green_color"[######################################]";
+
+echo $no_color"INSTALLING nodejs";
+sudo curl -sL https://deb.nodesource.com/setup_lts.x | sudo -E bash - >> $script_log_file 2>/dev/null
+sudo apt install nodejs -y >> $script_log_file 2>/dev/null
+echo $green_color"[SUCCESS]";
+echo $green_color"[######################################]";
+
+
+echo $no_color"FINALIZE INSTALLING";
+sudo apt-get autoremove -y >> $script_log_file 2>/dev/null
+sudo apt-get autoclean -y >> $script_log_file 2>/dev/null
+sudo apt-get update  >> $script_log_file 2>/dev/null
+echo $green_color"[SUCCESS]";
+echo $green_color"[######################################]";
+
+echo $green_color"[End]";
+echo $green_color"[####################]";
