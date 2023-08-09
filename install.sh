0464cce6c608d1538ae351a65c795ccc21088a65
MTGofa <gofa_25@hotmail.com>
2023-08-09 16:13:30 +0300

    save

--- install.sh
+++ install.sh
@@ -5,36 +5,32 @@ green_color="\033[1;32m"
 no_color="\033[0m"
 current_user=$(logname)
 
-
-echo $no_color"PREPAIRE INSTALLING";
+printf "# "$no_color"PREPAIRE INSTALLING";
 rm -rf /var/lib/dpkg/lock >> $script_log_file 2>/dev/null
 rm -rf /var/lib/dpkg/lock-frontend >> $script_log_file 2>/dev/null
 rm -rf /var/cache/apt/archives/lock >> $script_log_file 2>/dev/null
 sudo apt-get update  >> $script_log_file 2>/dev/null
-echo $green_color"[SUCCESS]";
-echo $green_color"[######################################]";
+printf $green_color" [SUCCESS]\n";
 
 
-echo $no_color"REMOVING APACHE";
+printf "# "$no_color"REMOVING APACHE";
 sudo apt-get purge apache -y >> $script_log_file 2>/dev/null
 sudo apt-get purge apache* -y >> $script_log_file 2>/dev/null
 sudo kill -9 $(sudo lsof -t -i:80) >> $script_log_file 2>/dev/null
 sudo kill -9 $(sudo lsof -t -i:443) >> $script_log_file 2>/dev/null
-echo $green_color"[SUCCESS]";
-echo $green_color"[######################################]";
+printf $green_color" [SUCCESS]\n";
 
 
-echo $no_color"INSTALLING NGINX";
+printf "# "$no_color"INSTALLING NGINX";
 sudo apt-get update   >> $script_log_file 2>/dev/null
 sudo apt install nginx -y >> $script_log_file 2>/dev/null
 sudo sed -i "/sites-enabled/a server_names_hash_bucket_size 64;\nclient_max_body_size 1000M;\nproxy_connect_timeout   3600;\nproxy_send_timeout      3600;\nproxy_read_timeout      3600;\nsend_timeout            3600;\nclient_body_timeout     3600;\nfastcgi_read_timeout 3600s;" /etc/nginx/nginx.conf >> $script_log_file 2>/dev/null
-echo $green_color"[SUCCESS]";
-echo $green_color"[######################################]";
+printf $green_color" [SUCCESS]\n";
 
 
 for version in 8.2 8.1 8.0 7.4;
 do
-    echo $no_color"INSTALLING PHP "$version;
+    printf "# "$no_color"INSTALLING PHP "$version;
     sudo apt-get update  >> $script_log_file 2>/dev/null
     sudo apt install lsb-release ca-certificates apt-transport-https software-properties-common -y >> $script_log_file 2>/dev/null
     sudo add-apt-repository ppa:ondrej/php -y >> $script_log_file 2>/dev/null
@@ -51,37 +47,32 @@ do
     sudo sed -i 's/memory_limit = 128/memory_limit = 12800/g' /etc/php/$version/fpm/php.ini >> $script_log_file 2>/dev/null
     sudo sed -i 's/user = www-data/user = '$current_user'/g' /etc/php/$version/fpm/pool.d/www.conf >> $script_log_file 2>/dev/null
     sudo service php$version-fpm restart >> $script_log_file 2>/dev/null
-    echo $green_color"[SUCCESS]";
-    echo $green_color"[######################################]";
+    printf $green_color" [SUCCESS]\n";
 done
 
 
-echo $green_color"[######################################]";
-echo $no_color"INSTALLING COMPOSER";
+printf "# "$no_color"INSTALLING COMPOSER";
 sudo apt-get update  >> $script_log_file 2>/dev/null
 sudo apt-get purge composer -y >> $script_log_file 2>/dev/null
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');" >> $script_log_file 2>/dev/null
 php composer-setup.php >> $script_log_file  2>/dev/null
 sudo mv composer.phar /usr/local/bin/composer >> $script_log_file 2>/dev/null
-echo $green_color"[SUCCESS]";
-echo $green_color"[######################################]";
+printf $green_color" [SUCCESS]\n";
 
 
-echo $no_color"RESTARTING NGINX";
+printf "# "$no_color"RESTARTING NGINX";
 sudo pkill -f nginx & wait $! >> $script_log_file 2>/dev/null
 sudo systemctl start nginx >> $script_log_file 2>/dev/null
 sudo service nginx restart >> $script_log_file 2>/dev/null
-echo $green_color"[SUCCESS]";
-echo $green_color"[######################################]";
+printf $green_color" [SUCCESS]\n";
 
 
-echo $no_color"PREPAIRE SSL CERTIFICATE";
+printf "# "$no_color"PREPAIRE SSL CERTIFICATE";
 sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/nginx-selfsigned.key -out /etc/ssl/certs/nginx-selfsigned.crt -subj "/C=EG/ST=London/L=London/O=Global Security/OU=IT Department/CN=example.com"
-echo $green_color"[SUCCESS]";
-echo $green_color"[######################################]";
+printf $green_color" [SUCCESS]\n";
 
 
-echo $no_color"Instaling phpmyadmin";
+printf "# "$no_color"Instaling phpmyadmin";
 sudo apt install unzip -y >> $script_log_file 2>/dev/null
 sudo wget -O /usr/share/phpmyadmin.zip https://files.phpmyadmin.net/phpMyAdmin/5.2.1/phpMyAdmin-5.2.1-all-languages.zip >> $script_log_file 2>/dev/null
 sudo unzip /usr/share/phpmyadmin.zip -d /usr/share/ >> $script_log_file 2>/dev/null
@@ -105,12 +96,11 @@ declare(strict_types=1);
 \$cfg['SaveDir'] = '';
 \$cfg['TempDir'] = '/tmp';
 EOT
-echo $green_color"[SUCCESS]";
-echo $green_color"[######################################]";
+printf $green_color" [SUCCESS]\n";
 exit
 
 
-echo $no_color"CREATING NGINX CONFIGURATION";
+printf "# "$no_color"CREATING NGINX CONFIGURATION";
 sudo rm -rf /etc/nginx/sites-available/default /etc/nginx/sites-enabled >> $script_log_file 2>/dev/null
 sudo ln -s /etc/nginx/sites-available /etc/nginx/sites-enabled >> $script_log_file 2>/dev/null
 sudo touch /etc/nginx/conf.d/default.conf >> $script_log_file 2>/dev/null
@@ -155,52 +145,45 @@ sudo bash -c "echo 'server {
 
 sudo chown -R ${USER}:${USER} /var/www >> $script_log_file 2>/dev/null
 sudo chmod -R 755 /var/www >> $script_log_file 2>/dev/null
-echo $green_color"[SUCCESS]";
-echo $green_color"[######################################]";
+printf $green_color" [SUCCESS]\n";
 
 
-echo $no_color"RESTARTING NGINX";
+printf "# "$no_color"RESTARTING NGINX";
 sudo pkill -f nginx & wait $! >> $script_log_file 2>/dev/null
 sudo systemctl start nginx >> $script_log_file 2>/dev/null
 sudo service nginx restart >> $script_log_file 2>/dev/null
-echo $green_color"[SUCCESS]";
-echo $green_color"[######################################]";
+printf $green_color" [SUCCESS]\n";
 
 if ! [ -x "$(command -v mysql)"  >> $script_log_file 2>/dev/null ]; then
-echo $no_color"INSTALLING MYSQL";
+printf "# "$no_color"INSTALLING MYSQL";
 sudo apt-get -qq install mysql-server  >> $script_log_file 2>/dev/null
-echo $green_color"[SUCCESS]";
-echo $green_color"[######################################]";
+printf $green_color" [SUCCESS]\n";
 fi
 
-echo $no_color"Configure MYSQL";
+printf "# "$no_color"Configure MYSQL";
 sudo mysql > /dev/null << EOF 
 ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '';
 FLUSH PRIVILEGES;
 EOF
-echo $green_color"[SUCCESS]";
-echo $green_color"[######################################]";
+printf $green_color" [SUCCESS]\n";
 
 
-echo $no_color"INSTALLING git";
+printf "# "$no_color"INSTALLING git";
 sudo apt-get update   >> $script_log_file 2>/dev/null
 sudo apt install git -y >> $script_log_file 2>/dev/null
-echo $green_color"[SUCCESS]";
-echo $green_color"[######################################]";
+printf $green_color" [SUCCESS]\n";
 
-echo $no_color"INSTALLING NPM";
+printf "# "$no_color"INSTALLING NPM";
 sudo apt install npm -y >> $script_log_file 2>/dev/null
-echo $green_color"[SUCCESS]";
-echo $green_color"[######################################]";
+printf $green_color" [SUCCESS]\n";
 
-echo $no_color"INSTALLING nodejs";
+printf "# "$no_color"INSTALLING nodejs";
 sudo curl -sL https://deb.nodesource.com/setup_lts.x | sudo -E bash - >> $script_log_file 2>/dev/null
 sudo apt install nodejs -y >> $script_log_file 2>/dev/null
-echo $green_color"[SUCCESS]";
-echo $green_color"[######################################]";
+printf $green_color" [SUCCESS]\n";
 
 
-echo $no_color"PREPAIRE bashrc";
+printf "# "$no_color"PREPAIRE bashrc";
 cat << EOT >> /home/$current_user/.bashrc 
 alias composer7.4='/usr/bin/php7.4 /usr/local/bin/composer'
 alias composer8.0='/usr/bin/php8.0 /usr/local/bin/composer'
@@ -213,13 +196,10 @@ alias checkout='git checkout'
 alias merge='git merge'
 EOT
 
-
-echo $no_color"FINALIZE INSTALLING";
+printf "# "$no_color"FINALIZE INSTALLING";
 sudo apt-get autoremove -y >> $script_log_file 2>/dev/null
 sudo apt-get autoclean -y >> $script_log_file 2>/dev/null
 sudo apt-get update  >> $script_log_file 2>/dev/null
-echo $green_color"[SUCCESS]";
-echo $green_color"[######################################]";
+printf $green_color" [SUCCESS]\n";
 
 echo $green_color"[End]";
-echo $green_color"[####################]";
