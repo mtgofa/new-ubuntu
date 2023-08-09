2621d26ee83a811545b9e794ed6c30f2774058c8
MTGofa <gofa_25@hotmail.com>
2023-08-09 16:23:58 +0300

    save

--- install.sh
+++ install.sh
@@ -36,11 +36,13 @@ do
     sudo add-apt-repository ppa:ondrej/php -y >> $script_log_file 2>/dev/null
     sudo apt-get update  >> $script_log_file 2>/dev/null
     sudo apt install php$version -y >> $script_log_file 2>/dev/null
-    
-    echo $no_color"INSTALLING PHP EXTENSIONS";
-    sudo apt install php$version openssl php$version-fpm php$version-common php$version-curl php$version-mbstring php$version-mysql php$version-xml php$version-zip php$version-gd php$version-cli php$version-xml php$version-imagick php$version-xml php$version-intl php-mysql -y >> $script_log_file 2>/dev/null
+    printf $green_color" [SUCCESS]\n";
 
-    echo $green_color"CHANGING PHP FPM UPLOAD VALUES";
+    printf "# "$no_color"INSTALLING PHP EXTENSIONS";
+    sudo apt install php$version openssl php$version-fpm php$version-common php$version-curl php$version-mbstring php$version-mysql php$version-xml php$version-zip php$version-gd php$version-cli php$version-xml php$version-imagick php$version-xml php$version-intl php-mysql -y >> $script_log_file 2>/dev/null
+    printf $green_color" [SUCCESS]\n";
+
+    printf "# "$green_color"CHANGING PHP FPM UPLOAD VALUES";
     sudo sed -i 's/post_max_size = 8M/post_max_size = 1000M/g' /etc/php/$version/fpm/php.ini >> $script_log_file 2>/dev/null
     sudo sed -i 's/upload_max_filesize = 2M/upload_max_filesize = 1000M/g' /etc/php/$version/fpm/php.ini >> $script_log_file 2>/dev/null
     sudo sed -i 's/max_execution_time = 30/max_execution_time = 300/g' /etc/php/$version/fpm/php.ini >> $script_log_file 2>/dev/null
@@ -78,7 +80,7 @@ sudo wget -O /usr/share/phpmyadmin.zip https://files.phpmyadmin.net/phpMyAdmin/5
 sudo unzip /usr/share/phpmyadmin.zip -d /usr/share/ >> $script_log_file 2>/dev/null
 sudo mv /usr/share/phpMyAdmin-5.2.1-all-languages /usr/share/phpmyadmin >> $script_log_file 2>/dev/null
 sudo rm /usr/share/phpmyadmin.zip >> $script_log_file 2>/dev/null
-cat << EOT >> /usr/share/phpmyadmin/config.inc.php 
+sudo cat << EOT >> /usr/share/phpmyadmin/config.inc.php 
 <?php
 declare(strict_types=1);
 \$cfg['blowfish_secret'] = '';
